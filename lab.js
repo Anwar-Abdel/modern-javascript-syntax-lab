c1990bf038102ba024addde86357c8fe1e51040a
Anwar-Abdel <anwarabdeljaber0@gmail.com>
2024-06-17 22:56:05 -0500

    feat: completed exercises 2-4

--- lab.js
+++ lab.js
@@ -14,3 +14,48 @@ const nums2 = nums.map((currentElement) => {
   });
   
 console.log(nums2); 
+
+
+
+//---------EXERCISE 2---------//
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+
+const [firstTopping, secondTopping] = pizzaToppings;
+
+console.log(firstTopping);
+console.log(secondTopping);
+
+
+//---------EXERCISE 3---------//
+
+// Given the following object, use destructuring to create variables `make` and `model` that will hold the respective values.
+
+const car = {
+    make: 'Audi',
+    model: 'q5',
+  };
+  
+  // Your code here
+
+  // Object destructuring:
+const { make, model } = car;
+
+console.log(make);
+console.log(model);
+
+
+//---------EXERCISE 4---------//
+
+// Duplicate the following array using the spread operator and assign it to `controversialPizzaToppings`. Then, log the variable.
+
+const pizzaToppings2 = ['Pineapple', 'Olives', 'Anchovies'];
+
+// Your code here
+const controversialPizzaToppings = [...pizzaToppings2];
+
+console.log(controversialPizzaToppings);
