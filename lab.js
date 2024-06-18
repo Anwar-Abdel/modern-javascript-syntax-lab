200e0162fc880f225fb50ef2e41f372815cbfc1e
Anwar-Abdel <anwarabdeljaber0@gmail.com>
2024-06-17 23:10:12 -0500

    feat: completed exercises 5-8

--- lab.js
+++ lab.js
@@ -59,3 +59,68 @@ const pizzaToppings2 = ['Pineapple', 'Olives', 'Anchovies'];
 const controversialPizzaToppings = [...pizzaToppings2];
 
 console.log(controversialPizzaToppings);
+
+
+
+//---------EXERCISE 5---------//
+
+// Duplicate the following object and spread its values into a new variable `myCar`.
+
+const car2 = {
+    make: 'Audi',
+    model: 'q5',
+  };
+  
+  // Change the `model` property of `myCar` to 'q7'. Log both objects.
+  
+  // Your code here
+
+const myCar2 = {...car2};
+
+console.log(myCar2)
+
+
+
+//---------EXERCISE 6---------//
+
+// Create an object named userProfile. 
+// Define a variable named propertyName and assign a string to it (like a username, age, or email). 
+// Use propertyName as a dynamic key in userProfile, assigning a relevant value.
+
+// Your code here
+
+let userProfile = {};
+
+let propertyName = "username";
+
+userProfile[propertyName] = "JohnDoe";
+
+console.log(userProfile);
+
+
+//---------EXERCISE 7---------//
+//Imported and Exported files
+
+
+
+
+
+//---------EXERCISE 8---------//
+
+// Create a function that takes two parameters, `noun` and `adjective`, both with the following respective default values:
+
+// 1. `cat`
+
+// 2. `white`
+
+// The function should log a sentence 'The cat is white.' by default. The function should substitute the appropriate parameters when supplied arguments.
+
+// Your code here
+
+function describeAnimal(noun = "cat", adjective = "white") {
+    console.log(`The ${noun} is ${adjective}.`);
+}
+
+// Examples
+describeAnimal();
+describeAnimal("turtle", "green");
